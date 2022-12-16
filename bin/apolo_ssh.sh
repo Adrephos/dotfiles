b35aec23f07fe44199f028096e341777fdd4395e
Adrephos <adrephos@gmail.com>
2022-12-15 23:20:38 -0500

    feat: add scripts for apolo ssh connection and xrandir config

---
+++ bin/apolo_ssh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+tmux new -d -s "apolo"
+tmux send-keys -t "apolo" C-z 'apolo_ssh_tmux' Enter
+tmux attach -t apolo
---
+++ bin/xrandr.sh
@@ -0,0 +1,9 @@
+#xrandr --output eDP1 --scale 0.8x0.8 --primary --mode 1920x1080 --pos 1536x0 --output HDMI1 --scale 1.2x1.2 --mode 1280x1024 --pos 0x0 
+#Apolo 
+# xrandr --output eDP1 --scale 1x1 --primary --mode 1920x1080 --pos 2016x756 --output HDMI1 --scale 1.4x1.4 --mode 1440x900 --pos 0x0
+xrandr --output eDP1 --scale 1x1 --primary --mode 1920x1080
+xrandr --output eDP1 --scale 1x1 --primary --mode 1920x1080 --pos 1792x0 --output HDMI1 --scale 1.4x1.4 --mode 1280x1024 --pos 0x0
+
+xrandr --output eDP1 --scale 1x1 --primary --mode 1920x1080 --pos 2016x756 --output HDMI1 --scale 1.4x1.4 --mode 1440x900 --pos 0x0
+
+xrandr --output eDP1 --scale 1x1 --primary --mode 1920x1080 --pos 1280x0 --output VIRTUAL1 --scale 1x1 --mode 1280x1024 --pos 0x0
